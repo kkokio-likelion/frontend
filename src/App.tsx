cdaec112f295654b070e93fbcf9fe503540462b8
wei756 <wei756fg@gmail.com>
2024-07-17 17:54:56 +0900

    기본 라우팅 정의

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import FrontPage from 'pages/front.page';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import MenuPage from 'pages/menu.page';
+import NotfoundPage from 'pages/notfound.page';
 import './App.css';
 
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<FrontPage />}></Route>
+          <Route path="/menu" element={<MenuPage />}></Route>
+          <Route path="*" element={<NotfoundPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
